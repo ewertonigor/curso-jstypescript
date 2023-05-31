3b9409a4ee0e732890f807ed291d1c072dc321e0
Ewerton Igor <121064919+ewertonigor@users.noreply.github.com>
2023-05-31 20:27:09 -0300

    Aula 11 - Aprendendo sobre union-types

---
+++ src/A0011-type-union-types/A0010-type-union-types.ts
@@ -0,0 +1,10 @@
+// Union Types você usa quando você quer ou um tipo ou outro tipo.
+function addOrConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(10, 20));
+console.log(addOrConcat('10', '20'));
+console.log(addOrConcat(10, '20'));
+console.log(addOrConcat('10', 20));
