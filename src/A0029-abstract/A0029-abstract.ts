925cce60af9c5798ec434d898ce5c7b71c16c5a3
Ewerton Igor <ewertonigor@icloud.com>
2023-06-07 13:11:11 -0300

    Aula 29 - Abstract

---
+++ src/A0029-abstract/A0029-abstract.ts
@@ -0,0 +1,45 @@
+// Abstract não vai mais permitir criar uma instância da classe
+export abstract class Personagem {
+  protected abstract emoji: string;
+
+  constructor(
+    protected nome: string,
+    protected ataque: number,
+    protected vida: number
+  ) {}
+
+  atacar(personagem: Personagem): void {
+    this.bordao();
+    personagem.perderVida(this.ataque);
+  }
+
+  perderVida(forcaAtaque: number): void {
+    this.vida -= forcaAtaque;
+    console.log(
+      `${this.emoji} -- ${this.nome} agora tem ${this.vida} de vida...`
+    );
+  }
+
+  abstract bordao(): void;
+}
+
+class Guerreira extends Personagem {
+  protected emoji = '\u{1F9DD}';
+  bordao(): void {
+    console.log(this.emoji + 'GUERREIRAAA AO ATAQUEEE!');
+  }
+}
+class Monstro extends Personagem {
+  protected emoji = '\u{1F9DF}';
+  bordao(): void {
+    console.log(this.emoji + 'AAAAARRHHGGGG');
+  }
+}
+
+const guerreira = new Guerreira('Guerreira', 100, 1000);
+const monstro = new Monstro('Monstro', 69, 1000);
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
+monstro.atacar(guerreira);
