2899c0914ade6ba6512983be05b89899b4753dab
Ewerton Igor <ewertonigor@icloud.com>
2023-06-16 19:33:38 -0300

    Aula 49 - Generics 7

---
+++ src/A0049-generics-part7/A0049-generics-part7.ts
@@ -0,0 +1,18 @@
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
+
+export function soma<T>(...args: T[]): number {
+  const retorno = args.reduce((sum, value) => {
+    if (isNumber(sum) && isNumber(value)) {
+      return sum + value;
+    }
+    return sum;
+  }, 0);
+
+  return retorno;
+}
+
+console.log(soma(2, 3, '6'));
+console.log(soma(2, 3, 5));
+console.log(soma('2', '3', '6'));
