4eaee780f266366cd5665dea60947d3980474361
Ewerton Igor <ewertonigor@icloud.com>
2023-06-19 13:34:57 -0300

    Aula 60 - Namespace

---
+++ src/A0060-namespaces/index.ts
@@ -0,0 +1,15 @@
+import { Pessoa } from '../A0023-herança/A0023-herança';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+// Oque estiver dentro do namespace não há como acessar de fora.
+namespace MeuNamespace {
+  const nome = 'Ewerton';
+
+  class PessoaDoNamespace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoa = new PessoaDoNamespace('Ewerton');
+
+  console.log(pessoa);
+}
