e37e98bf797e5714241789c4b47dfd6f5698895f
Ewerton Igor <ewertonigor@icloud.com>
2023-06-18 21:33:36 -0300

    Aula 58 - Decorator Property

---
+++ src/A0058-docorator-property/A0058-docorator-property.ts
@@ -0,0 +1,38 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+  let valorPropriedade: any;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      valorPropriedade = valor.split('').reverse().join('');
+    },
+  };
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
